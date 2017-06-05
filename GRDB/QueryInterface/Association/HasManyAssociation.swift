7f5e5584e514fd8e85e3057e8690666fff9ab83d
Gwendal Roué <gr@pierlis.com>
2017-06-05 19:25:05 +0200

    HasManyAssociation WIP

--- GRDB/QueryInterface/Association/HasManyAssociation.swift
+++ GRDB/QueryInterface/Association/HasManyAssociation.swift
@@ -79,38 +79,108 @@ extension TableMapping {
     }
 }
 
-extension TypedRequest {
-    public func including<Right>(_ association: HasManyAssociation<Fetched, Right>) -> GraphRequest<Self, Right> {
+extension QueryInterfaceRequest {
+    public func including<Right>(_ association: HasManyAssociation<Fetched, Right>) -> GraphRequest<Fetched, Right> {
         return GraphRequest(leftRequest: self, association: association)
     }
 }
 
 extension TableMapping {
-    public static func including<Right>(_ association: HasManyAssociation<Self, Right>) -> GraphRequest<QueryInterfaceRequest<Self>, Right> {
+    public static func including<Right>(_ association: HasManyAssociation<Self, Right>) -> GraphRequest<Self, Right> {
         return all().including(association)
     }
 }
 
-public struct GraphRequest<LeftRequest: TypedRequest, Right> {
-    var leftRequest: LeftRequest
-    let association: HasManyAssociation<LeftRequest.Fetched, Right>
+public struct GraphRequest<Left, Right> {
+    var leftRequest: QueryInterfaceRequest<Left>
+    let association: HasManyAssociation<Left, Right>
 }
 
-extension GraphRequest where LeftRequest.Fetched: RowConvertible & TableMapping, Right: RowConvertible {
-    public func fetchAll(_ db: Database) throws -> [(LeftRequest.Fetched, [Right])] {
+extension GraphRequest {
+    private func updatingLeftRequest(_ closure: (QueryInterfaceRequest<Left>) -> (QueryInterfaceRequest<Left>)) -> GraphRequest<Left, Right> {
+        return GraphRequest(leftRequest: closure(leftRequest), association: association)
+    }
+    
+    public func select(_ selection: SQLSelectable...) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.select(selection) }
+    }
+    
+    public func select(_ selection: [SQLSelectable]) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.select(selection) }
+    }
+    
+    public func select(sql: String, arguments: StatementArguments? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.select(sql: sql, arguments: arguments) }
+    }
+    
+    public func distinct() -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.distinct() }
+    }
+    
+    public func filter(_ predicate: SQLExpressible) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.filter(predicate) }
+    }
+    
+    public func filter(sql: String, arguments: StatementArguments? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.filter(sql: sql, arguments: arguments) }
+    }
+    
+    public func group(_ expressions: SQLExpressible...) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.group(expressions) }
+    }
+    
+    public func group(_ expressions: [SQLExpressible]) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.group(expressions) }
+    }
+    
+    public func group(sql: String, arguments: StatementArguments? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.group(sql: sql, arguments: arguments) }
+    }
+    
+    public func having(_ predicate: SQLExpressible) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.having(predicate) }
+    }
+    
+    public func having(sql: String, arguments: StatementArguments? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.having(sql: sql, arguments: arguments) }
+    }
+    
+    public func order(_ orderings: SQLOrderingTerm...) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.order(orderings) }
+    }
+    
+    public func order(_ orderings: [SQLOrderingTerm]) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.order(orderings) }
+    }
+    
+    public func order(sql: String, arguments: StatementArguments? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.order(sql: sql, arguments: arguments) }
+    }
+    
+    public func reversed() -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.reversed() }
+    }
+    
+    public func limit(_ limit: Int, offset: Int? = nil) -> GraphRequest<Left, Right> {
+        return updatingLeftRequest { $0.limit(limit, offset: offset) }
+    }
+}
+
+extension GraphRequest where Left: RowConvertible & TableMapping, Right: RowConvertible {
+    public func fetchAll(_ db: Database) throws -> [(Left, [Right])] {
         if association.rightColumns.count == 1 {
             let rightColumn = association.rightColumns[0]
-            var result: [(LeftRequest.Fetched, [Right])] = []
+            var result: [(Left, [Right])] = []
             var leftPrimaryKeys: [DatabaseValue] = []
             var resultIndexes : [DatabaseValue: Int] = [:]
             
-            let rowPrimaryKeyValue = try LeftRequest.Fetched.rowPrimaryKeyValue(db)
+            let rowPrimaryKeyValue = try Left.rowPrimaryKeyValue(db)
             let leftCursor = try Row.fetchCursor(db, leftRequest).enumerated()
             while let (index, leftRow) = try leftCursor.next() {
                 let leftPrimaryKey = rowPrimaryKeyValue(leftRow)
                 leftPrimaryKeys.append(leftPrimaryKey)
                 resultIndexes[leftPrimaryKey] = index
-                result.append((LeftRequest.Fetched(row: leftRow), []))
+                result.append((Left(row: leftRow), []))
             }
             
             if result.isEmpty {
