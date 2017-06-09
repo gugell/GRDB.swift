4208fd6ee48394779fae06d4bd637a785b3b2973
Gwendal Roué <gr@pierlis.com>
2017-06-09 17:29:05 +0200

    HasManyAssociationIncludingTests WIP

--- Tests/GRDBTests/HasManyAssociationIncludingTests.swift
+++ Tests/GRDBTests/HasManyAssociationIncludingTests.swift
@@ -7,93 +7,41 @@ import XCTest
     import GRDB
 #endif
 
+private typealias Author = AssociationFixture.Author
+private typealias Book = AssociationFixture.Book
+
 class HasManyAssociationIncludingTests: GRDBTestCase {
     
     // TODO: tests for left implicit row id, and compound keys
     
     func testSimplestRequest() throws {
-        struct Child : TableMapping, RowConvertible {
-            static let databaseTableName = "children"
-            let id: Int64
-            let parentId: Int64
-            let name: String
-            
-            init(row: Row) {
-                id = row.value(named: "id")
-                parentId = row.value(named: "parentId")
-                name = row.value(named: "name")
-            }
-        }
-        
-        struct Parent : TableMapping, RowConvertible {
-            static let databaseTableName = "parents"
-            let id: Int64
-            let name: String
-            
-            init(row: Row) {
-                id = row.value(named: "id")
-                name = row.value(named: "name")
-            }
-            
-            // The tested association
-            static let children = hasMany(Child.self)
-        }
-        
-        func assertEqual(_ graph: [(Parent, [Child])], _ expectedGraph: [(Parent, [Child])], file: StaticString = #file, line: UInt = #line) {
-            XCTAssertEqual(graph.count, expectedGraph.count, file: file, line: line)
-            for (pair, expectedPair) in zip(graph, expectedGraph) {
-                let (parent, children) = pair
-                let (expectedParent, expectedChildren) = expectedPair
-                XCTAssertEqual(parent.id, expectedParent.id, file: file, line: line)
-                XCTAssertEqual(parent.name, expectedParent.name, file: file, line: line)
-                XCTAssertEqual(children.count, expectedChildren.count, file: file, line: line)
-                for (child, expectedChild) in zip(children, expectedChildren) {
-                    XCTAssertEqual(child.id, expectedChild.id, file: file, line: line)
-                    XCTAssertEqual(child.parentId, expectedChild.parentId, file: file, line: line)
-                    XCTAssertEqual(child.name, expectedChild.name, file: file, line: line)
-                }
-            }
-        }
-        
         let dbQueue = try makeDatabaseQueue()
-        try dbQueue.inDatabase { db in
-            try db.create(table: "parents") { t in
-                t.column("id", .integer).primaryKey()
-                t.column("name", .text)
-            }
-            try db.create(table: "children") { t in
-                t.column("id", .integer).primaryKey()
-                t.column("parentId", .integer).references("parents")
-                t.column("name", .text)
-            }
-            
-            try db.execute("INSERT INTO parents (id, name) VALUES (?, ?)", arguments: [1, "a"])
-            try db.execute("INSERT INTO children (id, parentId, name) VALUES (?, ?, ?)", arguments: [1, 1, "a"])
-            try db.execute("INSERT INTO children (id, parentId, name) VALUES (?, ?, ?)", arguments: [2, 1, "b"])
-            try db.execute("INSERT INTO parents (id, name) VALUES (?, ?)", arguments: [2, "b"])
-            try db.execute("INSERT INTO children (id, parentId, name) VALUES (?, ?, ?)", arguments: [3, 2, "a"])
-            try db.execute("INSERT INTO children (id, parentId, name) VALUES (?, ?, ?)", arguments: [4, 2, "b"])
-            try db.execute("INSERT INTO parents (id, name) VALUES (?, ?)", arguments: [3, "a"])
-        }
+        try AssociationFixture().migrator.migrate(dbQueue)
         
         try dbQueue.inDatabase { db in
-            let graph = try Parent
-                .including(Parent.children)
+            let graph = try Author
+                .including(Author.books)
                 .fetchAll(db)
             
-            XCTAssertEqual(sqlQueries[sqlQueries.count - 2], "SELECT * FROM \"parents\"")
-            XCTAssertEqual(sqlQueries[sqlQueries.count - 1], "SELECT * FROM \"children\" WHERE (\"parentId\" IN (1, 2, 3))")
+            XCTAssertEqual(sqlQueries[sqlQueries.count - 2], "SELECT * FROM \"authors\"")
+            XCTAssertEqual(sqlQueries[sqlQueries.count - 1], "SELECT * FROM \"books\" WHERE (\"authorId\" IN (1, 2, 3, 4))")
             
-            assertEqual(graph, [
-                (Parent(row: ["id": 1, "name": "a"]), [
-                    Child(row: ["id": 1, "parentId": 1, "name": "a"]),
-                    Child(row: ["id": 2, "parentId": 1, "name": "b"]),
+            assertMatch(graph, [
+                (["id": 1, "name": "Gwendal Roué"], []),
+                (["id": 2, "name": "J. M. Coetzee"], [
+                    ["id": 1, "authorId": 2, "title": "Foe", "year": 1986],
+                    ["id": 2, "authorId": 2, "title": "Three Stories", "year": 2014],
                     ]),
-                (Parent(row: ["id": 2, "name": "b"]), [
-                    Child(row: ["id": 3, "parentId": 2, "name": "a"]),
-                    Child(row: ["id": 4, "parentId": 2, "name": "b"]),
+                (["id": 3, "name": "Herman Melville"], [
+                    ["id": 3, "authorId": 3, "title": "Moby-Dick", "year": 1851],
                     ]),
-                (Parent(row: ["id": 3, "name": "a"]), []),
+                (["id": 4, "name": "Kim Stanley Robinson"], [
+                    ["id": 4, "authorId": 4, "title": "New York 2140", "year": 2017],
+                    ["id": 5, "authorId": 4, "title": "2312", "year": 2012],
+                    ["id": 6, "authorId": 4, "title": "Blue Mars", "year": 1996],
+                    ["id": 7, "authorId": 4, "title": "Green Mars", "year": 1994],
+                    ["id": 8, "authorId": 4, "title": "Red Mars", "year": 1993],
+                    ]),
                 ])
         }
     }
