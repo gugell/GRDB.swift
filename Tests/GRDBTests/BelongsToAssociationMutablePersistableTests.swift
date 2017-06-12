0090d240d7991cd21218715513e1b388042774e9
Gwendal Roué <gr@pierlis.com>
2017-06-12 08:04:56 +0200

    BelongsToAssociationMutablePersistableTests WIP

---
+++ Tests/GRDBTests/BelongsToAssociationMutablePersistableTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+#if GRDBCIPHER
+    import GRDBCipher
+#elseif GRDBCUSTOMSQLITE
+    import GRDBCustomSQLite
+#else
+    import GRDB
+#endif
+
+private typealias Author = AssociationFixture.Author
+private typealias Book = AssociationFixture.Book
+
+class BelongsToAssociationMutablePersistableTests: GRDBTestCase {
+}
